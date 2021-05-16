e9cd4672386a6e5b0140d3ef4d4bb01e2325128f
Ian Arbuckle <ian.arbuckle18@gmail.com>
2021-05-16 13:29:06 +0100

    - Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,12 @@
+pluginManagement {
+    repositories {
+        google()
+        jcenter()
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Craftie"
+include(":androidApp")
+include(":shared")
