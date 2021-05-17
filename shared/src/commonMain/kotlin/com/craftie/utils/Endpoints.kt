3b8040192d7d4d3bfef09edf604119a21fddfa1c
Ian Arbuckle <ian.arbuckle18@gmail.com>
2021-05-17 18:57:38 +0100

    * Fix typo

--- shared/src/commonMain/kotlin/com/craftie/utils/Endpoints.kt
+++ shared/src/commonMain/kotlin/com/craftie/utils/Endpoints.kt
@@ -4,6 +4,6 @@ object Endpoints {
     private const val BASE_URL = "https://craftie-api.herokuapp.com/api/v1"
 
     const val BEERS_ENDPOINT = "$BASE_URL/beers"
-    const val BEERS_FEATURED_ENDPOINT = "$BEERS_ENDPOINT/featued"
+    const val BEERS_FEATURED_ENDPOINT = "$BEERS_ENDPOINT/featured"
     const val BREWERIES_ENDPOINT = "$BASE_URL/breweries"
 }
