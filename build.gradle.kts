53045e9eee2df54f0def8362f8e9182638f20642
Ian Arbuckle <ian.arbuckle18@gmail.com>
2021-10-21 15:46:59 +0100

    * Update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,8 +6,8 @@ buildscript {
     }
     dependencies {
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:${Versions.kotlin}")
-        classpath("com.android.tools.build:gradle:7.0.2")
-        classpath("com.google.dagger:hilt-android-gradle-plugin:2.37")
+        classpath("com.android.tools.build:gradle:7.0.3")
+        classpath("com.google.dagger:hilt-android-gradle-plugin:2.39.1")
         classpath("org.jetbrains.kotlin:kotlin-serialization:${Versions.kotlin}")
         classpath("com.google.gms:google-services:4.3.10")
     }
